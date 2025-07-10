a7058a5cf733f822d61f583775b5129536aad2c8
Luis Parente <lgsp25@hotmail.com>
2025-07-10 19:58:12 -0300

    Exception handling

---
+++ src/main/java/com/devsuperior/DSPosts/model/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.devsuperior.DSPosts.model.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.DSPosts.model.controllers.exceptions.StandardError;
+import com.devsuperior.DSPosts.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), "Not found", e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
