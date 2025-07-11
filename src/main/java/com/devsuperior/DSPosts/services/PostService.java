0bf5c130fc81101120e8dc650d8369eb24d2abb5
Luis Parente <lgsp25@hotmail.com>
2025-07-11 20:39:56 -0300

    Fix import

--- src/main/java/com/devsuperior/DSPosts/services/PostService.java
+++ src/main/java/com/devsuperior/DSPosts/services/PostService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.DSPosts.model.dto.PostDTO;
 import com.devsuperior.DSPosts.model.entities.Post;
